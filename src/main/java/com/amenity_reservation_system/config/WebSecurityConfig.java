df8d19ab3c1af2c4a3f0f00ea1ed1ef07b509f03
Yiğit Kemal Erinç <yigit.erinc@ozu.edu.tr>
2021-09-28 22:59:26 +0300

    Make Spring Security ignore h2-console endpoints

--- src/main/java/com/amenity_reservation_system/config/WebSecurityConfig.java
+++ src/main/java/com/amenity_reservation_system/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.amenity_reservation_system.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,16 +17,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    private final String[] SECURITY_IGNORED_ENDPOINTS = {
+            "/h2-console/**", "/h2/**", "/webjars/**"
+    };
+
     public WebSecurityConfig(UserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(SECURITY_IGNORED_ENDPOINTS);
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/webjars/**", "/h2-console/**").permitAll()
+                .antMatchers("/")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
